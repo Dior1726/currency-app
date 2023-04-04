78f12528c534ab78b9adfb8f0820f479b39c45d2
Dior1726 <muxammad_909@mail.ru>
2023-04-04 11:47:09 +0600

    refactor: App.vue and HomeView.vue

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,12 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <div class="wrapper">
     <header class="header">
-      <div class="container">
-        <h1>Currency app</h1>
+      <h1>Currency app</h1>
 
-        <nav>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-        </nav>
-      </div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
     </header>
 
     <main class="main">
@@ -22,49 +20,4 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
-.wrapper {
-  width: 100%;
-  min-height: 100vh;
-}
-
-.header {
-  width: 100%;
-  border-bottom: 1px solid var(--color-border);
-}
-
-.header .container {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px;
-}
-
-.header h1 {
-  font-size: 24px;
-  color: hsla(160, 100%, 37%, 1);
-  font-weight: 500;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="currency-card">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aut, iure adipisci atque assumenda
+    quaerat tenetur quas omnis ratione eligendi dolorum. Molestias, velit? Nemo porro, reiciendis et
+    totam non sint!
+  </div>
 </template>
+
+<style scoped>
+.currency-card {
+  max-width: 800px;
+  padding: 40px;
+  border-radius: 12px;
+  border: 1px solid var(--color-border);
+  background: rgba(255, 255, 255, 0.1);
+  color: white;
+}
+</style>
